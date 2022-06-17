8d54b6462156ec3833255d90fec48779f08ef9cf
Diego Staniescki <Diego.stk@live.com>
2022-06-16 22:45:42 -0300

    Alterar dist

--- resources/frontend/src/environments/environment.local.ts
+++ resources/frontend/src/environments/environment.local.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  APP_URL: "http://ec2-user@ec2-18-220-39-42.us-east-2.compute.amazonaws.com",
-  //APP_URL: "http://localhost:8081/projeto_tcc/futeboldesalao-Upload-arquivos/public",
+  APP_URL: "",
 }
