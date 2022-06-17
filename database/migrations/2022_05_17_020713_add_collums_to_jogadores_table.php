c4965b101ab17681649c514bf0097a8abfde6732
Diego Staniescki <Diego.stk@live.com>
2022-06-16 23:09:02 -0300

    Alterar dist

---
+++ database/migrations/2022_05_17_020713_add_collums_to_jogadores_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCollumsToJogadoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('jogadores', function (Blueprint $table) {
+            $table->longText('imgjogador');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('jogadores', function (Blueprint $table) {
+            //
+        });
+    }
+}
